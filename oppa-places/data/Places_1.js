4f9b78d845ef9d765c6efd91868fdb5a7a481420
Marigold0103 <jeonghwan.choi0103@gmail.com>
2025-01-04 18:14:17 +0200

    Update Places_1.js

--- oppa-places/data/Places_1.js
+++ oppa-places/data/Places_1.js
@@ -1,1 +1,93 @@
-var json_Places_1 = {"type":"FeatureCollection","name":"Places_1","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"title":"Every story has a beginning...","description":"Where a shy boy met a talkative girl and asked her number"},"geometry":{"type":"Point","coordinates":[26.720987494124145,58.379500595701025]}},{"type":"Feature","properties":{"title":"Fake romantic news","description":"Where are all those singers in the streets and romantic people gifting flowers? Not in here but at least they have aperitivo!"},"geometry":{"type":"Point","coordinates":[9.191099442889238,45.46415674580674]}},{"type":"Feature","properties":{"title":"More than a movie","description":"Where the cinema can disappoint lovers, but a romantic stroll along the river and through the park is a unique experience"},"geometry":{"type":"Point","coordinates":[24.105073320970366,56.949453609334483]}},{"type":"Feature","properties":{"title":"Singing ABBA in the rain","description":"A dancing (and singing) queen meets a Nobel prize for romance"},"geometry":{"type":"Point","coordinates":[18.070325549178953,59.326376988907633]}},{"type":"Feature","properties":{"title":"A kiss from the best Lake in Italy","description":"Well... what's better than this?"},"geometry":{"type":"Point","coordinates":[8.562877437936388,45.755184613723543]}},{"type":"Feature","properties":{"title":"Gita fuori pOrta","description":"Literally, day trip in a place with benches with perfect view and good music (on the airpods)"},"geometry":{"type":"Point","coordinates":[8.405727239624795,45.798072698062079]}},{"type":"Feature","properties":{"title":"The luxury of good feelings","description":"Sunny november days don't stop explorer of the most famous (but less beautiful) lake in Italy"},"geometry":{"type":"Point","coordinates":[9.075959293979373,45.813660186919577]}},{"type":"Feature","properties":{"title":"Boring city","description":"...but good food and wine!"},"geometry":{"type":"Point","coordinates":[9.69543,45.05279]}}]}
+var json_Places_1 = {
+  type: "FeatureCollection",
+  name: "Places_1",
+  crs: { type: "name", properties: { name: "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        title: "Every story has a beginning...",
+        description:
+          "Where a shy boy met a talkative girl and asked her number",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [26.720987494124145, 58.379500595701025],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        title: "Fake romantic news",
+        description:
+          "Where are all those singers in the streets and romantic people gifting flowers? Not in here but at least they have aperitivo!",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [9.191099442889238, 45.46415674580674],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        title: "More than a movie",
+        description:
+          "Where the cinema can disappoint lovers, but a romantic stroll along the river and through the park is a unique experience",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [24.105073320970366, 56.949453609334483],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        title: "Singing ABBA in the rain",
+        description:
+          "A dancing (and singing) queen meets a Nobel prize for romance",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [18.070325549178953, 59.326376988907633],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        title: "A kiss from the best Lake in Italy",
+        description: "Well... what's better than this?",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [8.562877437936388, 45.755184613723543],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        title: "Gita fuori pOrta",
+        description:
+          "Literally, day trip in a place with benches with perfect view and good music (on the airpods)",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [8.405727239624795, 45.798072698062079],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        title: "The luxury of good feelings",
+        description:
+          "Sunny november days don't stop explorer of the most famous (but less beautiful) lake in Italy",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [9.075959293979373, 45.813660186919577],
+      },
+    },
+    {"type":"Feature","properties":{"title":"Boring city","description":"...but good food and wine!"},"geometry":{"type":"Point","coordinates":[9.69543,45.05279]}},
+    {"type":"Feature","properties":{"title":"The city where Beauty and Food Beats Piacenza and Cremona","description":"Good food and music! It's also our first celebration of the New Year 2025"},"geometry":{"type":"Point","coordinates":[10.3292, 44.7816]}},
+    {"type":"Feature","properties":{"title":"Home of Stradivarius Violins","description":"The hometown of the beautiful Stradivarius violins, even more stunning than Piacenza."},"geometry":{"type":"Point","coordinates":[10.0261, 45.1365]}}]}
+  ],
+};
